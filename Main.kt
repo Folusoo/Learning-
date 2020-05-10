47138210947f6c936fea29dcf057d805fd69d066
Folusoo <64383921+Folusoo@users.noreply.github.com>
2020-05-10 12:42:28 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -11,3 +11,16 @@ fun main() {
     val person = Person()
     person.printInfo()
 }
+
+
+//If and Else Expression
+
+/*   val age : Int = 16
+    if (age < 10) {
+        println("You are too young to watch this movie")
+    } else if (age < 13) {
+        println("You may watch this movie with your parents")
+    } else {
+        println("You can watch this movie")
+}
+*/
